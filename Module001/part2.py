845648b292b406848a012dd121842a107ff12ec0
avkozobrod <avkozobrod@gmail.com>
2023-11-05 20:25:03 +0300

    update file

--- Module001/part2.py
+++ Module001/part2.py
@@ -10,3 +10,33 @@ def file_size(a):
     print(int(a)//1024)
 #file_size(2340)
 
+def number_segments(A,B):
+    print(A//B)
+#number_segments(56,15)
+
+def residual(A,B):
+    print(A%B)
+#residual(36,9)
+def two_digit_number(a):
+    print(a//10)
+    print(a%10)
+#two_digit_number(34)
+
+def two_digit_number_sum_and_product(a):
+    print((a//10)+(a%10))
+    print((a//10)*(a%10))
+#two_digit_number_sum_and_product(27)
+
+def two_digit_reverse(a):
+    #print(int(str(a%10)+str(a//10)))
+    print((a%10)*10+(a//10))
+#two_digit_reverse(63)
+
+def three_digit_nuber_first_digit(a):
+    print(a//100)
+#three_digit_nuber_first_digit(623)
+
+def three_digit_nuber_last_and_middle_digit(a):
+    print(a%10)
+    print((a//10)%10)
+#three_digit_nuber_last_and_middle_digit(743)
