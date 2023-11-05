7ae11c1918ed419c3c66c36a8e6ca397346b60fe
avkozobrod <avkozobrod@gmail.com>
2023-11-05 21:58:14 +0300

    update file

---
+++ Module001/part3.py
@@ -0,0 +1,39 @@
+def exercise_1(a):
+    print(a>0)
+#exercise_1(5)
+
+def exercise_2(a):
+    print((a%2)!=0)
+#exercise_2(45)
+
+def exercise_3(a):
+    print((a%2)==0)
+#exercise_3(4)
+
+def exercise_4(a,b):
+    print(a>2 and b<=3)
+#exercise_4(4,3)
+
+def exercise_5(a,b):
+    print(a>=0 or b<-2)
+#exercise_5(3,3)
+
+def exercise_6(a,b,c):
+    print(a<b<c)
+#exercise_6(3,4,5)
+
+def exercise_7(a,b,c):
+    print(b>a and b<c)
+#exercise_7(3,4,5)
+
+def exercise_8(a,b):
+    print((a%2)!=0 and (b%2)!=0)
+#exercise_8(3,5)
+
+def exercise_9(a,b):
+    print((a%2)!=0 or (b%2)!=0)
+#exercise_9(5,4)
+
+def exercise_10(a,b):
+    print(((a%2)!=0 and (b%2)==0) or ((a%2)==0 and (b%2)!=0))
+#exercise_10(5,4)
